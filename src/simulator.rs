bfb300c0b6b92c82c014a30044e898d40b90b9ae
Sniffy Gumbles <155901370+SGumbles@users.noreply.github.com>
2026-03-06 10:49:37 -0800

    Start writing a few tests for the simulator. Rust's matching stuff is crazy, it's so handy.

--- src/simulator.rs
+++ src/simulator.rs
@@ -12,3 +12,22 @@ pub struct SimulatorResults{
 pub fn run_simulator(params:SimulatorParameters) -> SimulatorResults {
     SimulatorResults { monthly_payments: params.mortgage / (params.term_length_years as f64 * 12.0) }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_payment_test() {
+        let r = run_simulator(SimulatorParameters { 
+            mortgage: 500000.0, 
+            annual_interest_rate: 5.2, 
+            term_length_years: 35, 
+            pay_down_principal_first: false
+        });
+        assert!( match r.monthly_payments {
+            1190.45..1190.48 => true,
+            _ => false
+        } );
+    }
+}
