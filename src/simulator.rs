e3d96b1a0c8f1e9934df3e0e505a1465569dc6bf
Sniffy Gumbles <155901370+SGumbles@users.noreply.github.com>
2026-03-06 21:47:03 -0800

    Ok, banged out the basics of figuring out our monthly payments numerically. I knew this would be the right way to go long term rather than figuring out some crazy formula for every change in payments I wanted to make. Turns out there is a different way of compounding interest in the U.S vs Canada, which would have normally been a huge pain, but sloting in this difference was super easy.

--- src/simulator.rs
+++ src/simulator.rs
@@ -9,8 +9,88 @@ pub struct SimulatorResults{
     pub monthly_payments : f64
 }
 
+pub struct MonthlyRecord {
+    pub amount_paid_to_principal: f64,
+    pub amount_paid_to_interest: f64,
+    pub remaining_principal: f64
+}
+
+const MAX_STEP_SIZE:f64 = 5.0;
+
+fn compute_monthly_interest_rate(annual_interest_rate: f64, canadian_style: bool) -> f64 {
+    if canadian_style {
+        (1.0 + (annual_interest_rate / 100.0 / 2.0)).powf(1.0/6.0) - 1.0
+    } else {
+        annual_interest_rate / 100.0 / 12.0
+    } 
+}
+
+fn how_long_to_pay(per_month:f64, annual_interest:f64, principal:f64) -> f64 {
+    let monthly_interest = compute_monthly_interest_rate(annual_interest, true);
+
+    let mut months = 0;
+    let mut remaining_principal = principal;
+    loop {
+        let how_much_we_pay_in_interest = monthly_interest * remaining_principal;
+        if (per_month - how_much_we_pay_in_interest) < 0.0 {
+            // Impossible to repay loan
+            return 1000.0;
+        }
+        remaining_principal = remaining_principal - (per_month - how_much_we_pay_in_interest);
+        if remaining_principal < 0.0 {
+            return months as f64;
+        }
+        months = months + 1;
+    }
+}
+
+pub fn compute_monthly_payments(principal:f64, annual_interest: f64, term_length_years: u32) -> f64 {
+    let term_length_months = (term_length_years as f64) * 12.0;
+    // Pay just the interest per month AND some small step size towards the principal
+    let mut payment_guess = (principal * compute_monthly_interest_rate(annual_interest,true)) + MAX_STEP_SIZE;
+    
+    struct Guess {
+        payment:f64,
+        term_length:f64
+    }
+
+    let mut previous_guess : Option<Guess> = None;
+    
+    loop {
+        let term_length_guess = how_long_to_pay(payment_guess, annual_interest, principal);
+        if (term_length_guess - term_length_months) == 0.0 {
+            return payment_guess
+        } else {
+            let next_payment_guess = match previous_guess {
+                None => {
+                    if term_length_guess > term_length_months {
+                        payment_guess * 1.05
+                    } else {
+                        payment_guess * 0.95
+                    }
+                }
+                Some(p) => {
+                    let delta_payment = payment_guess - p.payment;
+                    let delta_term = (term_length_guess - p.term_length) as f64;
+                    let term_deriv = delta_term / delta_payment;
+                    let delta_error = (term_length_months - term_length_guess) as f64;
+                    let delta_correction = delta_error * (1.0 / term_deriv);
+                    payment_guess + delta_correction.clamp(-MAX_STEP_SIZE, MAX_STEP_SIZE)
+                }
+            };
+            previous_guess = Some(Guess{
+                payment:payment_guess,
+                term_length:term_length_guess
+            });
+            payment_guess = next_payment_guess;
+        }
+    }
+}
+
 pub fn run_simulator(params:SimulatorParameters) -> SimulatorResults {
-    SimulatorResults { monthly_payments: params.mortgage / (params.term_length_years as f64 * 12.0) }
+    SimulatorResults{
+        monthly_payments : 24.0
+    }
 }
 
 #[cfg(test)]
@@ -18,15 +98,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn compute_term_length_test() {
+        let how_many_months = how_long_to_pay( 
+            1787.75,
+             5.25, 
+            300000.0);
+        assert!(how_many_months == 300.0);
+    }
+
+    #[test]
+    fn compute_monthly_payments_test() {
+        let monthly = compute_monthly_payments( 
+            300000.0, 
+             5.25, 
+            25);
+        assert!( match monthly {
+            1787.0..1789.0 => true,
+            _ => false
+        } );
+    }
+
+    #[test]
     fn simple_payment_test() {
         let r = run_simulator(SimulatorParameters { 
             mortgage: 500000.0, 
-            annual_interest_rate: 5.2, 
-            term_length_years: 35, 
+            annual_interest_rate: 5.25, 
+            term_length_years: 25, 
             pay_down_principal_first: false
         });
         assert!( match r.monthly_payments {
-            1190.45..1190.48 => true,
+            2979.0..2981.0 => true,
             _ => false
         } );
     }
