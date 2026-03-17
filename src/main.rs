4eaad9f087ce569799cabcd96821d94c7bdca47d
Sniffy Gumbles <155901370+SGumbles@users.noreply.github.com>
2026-03-16 19:25:23 -0700

    - Starting to block out interface with tailwind

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use dioxus::{prelude::*};
 
-static CSS : Asset = asset!("/assets/main.css");
-
 fn main() {
     dioxus::launch(App);
 }
@@ -9,10 +7,48 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx!{
-        document::Stylesheet { href : CSS}
-        h1 {
-            class: "text-lg",
-            "Hello"
+        Stylesheet { href: asset!("/assets/tailwind.css") }
+        div {
+            class: "flex bg-slate-500 text-amber-50",
+            div { 
+                class: "size-32 flex flex-col justify-center items-center",
+                p{
+                    class: "font-bold text-4xl",
+                    "House"
+                }
+                p{
+                    class: "font-light",
+                    "Or"
+                }
+                p{
+                    class: "font-bold text-4xl",
+                    "Hodl"
+                }  
+            }
+            div { 
+                class:"size-32 grow bg-slate-600 p-2 flex flex-col justify-around",
+                p {  
+                    "Is it better to own a house with a mortgage, or pour that money into an index fund and hope the market doesn't implode?"
+                } 
+                p { 
+                    "What does each timeline look like under different circumstances?"
+                }
+                p { 
+                    "Use this calculator to model out both scenarios"
+                }
+            }
+        }
+
+        div {  
+            class: "flex text-amber-50",
+            div {
+                class: "bg-mauve-800 grow",
+                "Yo"
+            }
+            div {
+                class: "bg-mist-800 grow",
+                "Hello"
+            }
         }
     }
 }
